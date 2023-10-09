8e14d1617dc52c65f6af59f6a8cca04d8ef7176a
Azuyamat <blaneyderek@gmail.com>
2023-10-09 18:08:57 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "com.azuyamat"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("dev.kord:kord-core:0.11.1")
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
+    implementation("org.mongodb:mongodb-driver-kotlin-sync:4.10.2")
+    implementation("org.slf4j:slf4j-nop:2.0.9")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
